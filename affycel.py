c6f4f96bc5b255b24be58cfd9e69db0c6b8fc9d5
Vincent Davis <vincentdaviscom@gmail.com>
2010-02-25 21:12:15 -0700

    Fixed mistake in __name__ == "__main__": , it was in the wrong place Fixed issue with the end of areader when readin in modified section

--- affycel.py
+++ affycel.py
@@ -6,14 +6,9 @@ import numpy
 #running this will import the example.cel file
 # It is the way for testing
 
+
+
 class affycel:
-    
-    if __name__ == "__main__":        
-        a = affycel()
-        a.read_cel("example.CEL")
-        testlist = (self.filename, self.version, self.header.items(), self.intensityCells, self.intensity[:5], self.masksCells, self.masks, self.outliersCells, self.outliers[:5], self.modifiedCells, self.modified[:5])
-        for test in testlist:
-            print str(test), test
 
     def _int_(self, filename, version, header, intensityCells, intensity, maskscells, masks, outlierCells, outliers, modifiedCells, modified):
         self.filename = filename
@@ -26,19 +21,12 @@ class affycel:
         self.outliersCells = outlierCells
         self.outliers = outliers
         self.modifiedCells = modifiedCells
-        self.modfied = modified
+        self.modified = modified
         self.custom = {} # plan to allow a custom section to be added to the CEL file
         
     def read_cel(self, filename):
         reader = csv.reader(open(filename, "U"),delimiter='\t')
-        self.filename = os.path.split(filename)[1]
-        rsel = {}
-        rsel['[CEL]'] = Rcel
-        rsel['[HEADER]']= Rheader
-        rsel['[INTENSITY]']= Rintensity
-        rsel['[MASKS]']= Rmasks
-        rsel['[OUTLIERS]']= Routliers
-        rsel['[MODIFIED]']= Rmodified        
+        self.filename = os.path.split(filename)[1]        
         
         def read_selector(areader):
             for row in areader:
@@ -51,7 +39,7 @@ class affycel:
             if '[CEL]' in row: #row passed in should contain '[CEL]'
                 for row in areader: #Skips '[CEL]' row that was passed in
                     if row: # skips blank rows
-                        print 'cell', row
+                        #print 'cell', row
                         if not any(("[HEADER]" in row, "[INTENSITY]" in row, "[MASKS]" in row, "[OUTLIERS]" in row, "[MODIFIED]" in row)):
                             self.version = int(row[0].partition('=')[2])
                             #print self.version
@@ -116,7 +104,6 @@ class affycel:
             
         def Rmodified(row, areader):
             data = []
-            modifiedtype = [('x', int), ('y', int), ('ORIGMEAN', float)]
             if "[MODIFIED]" in row:
                             row = areader.next() # moves to the row after "[INTENSITY]"
                             self.modifiedCells = int(row[0].partition('=')[2]) #gets the number of intensities
@@ -124,13 +111,25 @@ class affycel:
             for row in reader:
                 if row:
                     if not any(("[CEL]" in row, "[HEADER]" in row, "[INTESITY]" in row, "[MASKS]" in row, "[OUTLIERS]" in row)):
+                        print 'modified1'
                         data.append(tuple(row))
-                    else:
-                        self.modified[masks] = numpy.array(data, [('x', int), ('y', int), ('origmean', numpy.float64)] )
-                        #rsel[row[0]](row, areader)  This should be the last item in the file
+                    #else, there is no else statment when there are now more rows continue on to convert data to array
+            self.modified = numpy.array(data, [('x', int), ('y', int), ('origmean', numpy.float64)] )
+            #rsel[row[0]](row, areader)  This should be the last item in the file
+            
+        rsel = {}
+        rsel['[CEL]'] = Rcel
+        rsel['[HEADER]']= Rheader
+        rsel['[INTENSITY]']= Rintensity
+        rsel['[MASKS]']= Rmasks
+        rsel['[OUTLIERS]']= Routliers
+        rsel['[MODIFIED]']= Rmodified
         
         read_selector(reader)
-
         
-#a = affycel()
-#a.read_cel("example.CEL")
+if __name__ == "__main__": 
+        a = affycel()
+        a.read_cel('example.CEL')
+        testlist = (a.filename, a.version, a.header.items(), a.intensityCells, a.intensity[:5], a.masksCells, a.masks, a.outliersCells, a.outliers[:5], a.modifiedCells, a.modified[:5])
+        for test in testlist:
+            print 'Test', test
