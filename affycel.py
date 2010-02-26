3a7e91fca239495dfeecdffcc363727378b3a2cc
Vincent Davis <vincentdaviscom@gmail.com>
2010-02-25 20:36:50 -0700

    Completed conversion to numpy record arrays for MASKS, INTENSITIES, OUTLIERS and MODIFIED

--- affycel.py
+++ affycel.py
@@ -7,6 +7,14 @@ import numpy
 # It is the way for testing
 
 class affycel:
+    
+    if __name__ == "__main__":        
+        a = affycel()
+        a.read_cel("example.CEL")
+        testlist = (self.filename, self.version, self.header.items(), self.intensityCells, self.intensity[:5], self.masksCells, self.masks, self.outliersCells, self.outliers[:5], self.modifiedCells, self.modified[:5])
+        for test in testlist:
+            print str(test), test
+
     def _int_(self, filename, version, header, intensityCells, intensity, maskscells, masks, outlierCells, outliers, modifiedCells, modified):
         self.filename = filename
         self.version = version
@@ -19,12 +27,25 @@ class affycel:
         self.outliers = outliers
         self.modifiedCells = modifiedCells
         self.modfied = modified
-        self.custom = {}
+        self.custom = {} # plan to allow a custom section to be added to the CEL file
         
     def read_cel(self, filename):
         reader = csv.reader(open(filename, "U"),delimiter='\t')
         self.filename = os.path.split(filename)[1]
-        # continu past these def to the next step
+        rsel = {}
+        rsel['[CEL]'] = Rcel
+        rsel['[HEADER]']= Rheader
+        rsel['[INTENSITY]']= Rintensity
+        rsel['[MASKS]']= Rmasks
+        rsel['[OUTLIERS]']= Routliers
+        rsel['[MODIFIED]']= Rmodified        
+        
+        def read_selector(areader):
+            for row in areader:
+                if row:
+                    if any(("[CEL]" in row, "[HEADER]" in row, "[INTENSITY]" in row, "[MASKS]" in row, "[OUTLIERS]" in row, "[MODIFIED]" in row)):
+                        rsel[row[0]](row, areader)
+                    else: print '*****something went wrong*******'
 
         def Rcel(row, areader):
             if '[CEL]' in row: #row passed in should contain '[CEL]'
@@ -33,7 +54,7 @@ class affycel:
                         print 'cell', row
                         if not any(("[HEADER]" in row, "[INTENSITY]" in row, "[MASKS]" in row, "[OUTLIERS]" in row, "[MODIFIED]" in row)):
                             self.version = int(row[0].partition('=')[2])
-                            print self.version
+                            #print self.version
                             #self.version = row
                         else: 
                             rsel[row[0]](row, areader) # Go to correct section
@@ -49,21 +70,17 @@ class affycel:
                             rsel[row[0]](row, areader) # Go to correct section
                 
         def Rintensity(row, areader):
-            print 'start intencity', row 
+            #print 'start intencity', row 
             data = []
             if "[INTENSITY]" in row: #row passed in should contain '[INTENSITY]'
                 row = areader.next() # moves to the row after "[INTENSITY]"
                 self.intensityCells = int(row[0].partition('=')[2]) #gets the number of intensities
                 areader.next() #skips the colmn headings
-                print 'cont', row
                 for row in reader:
                     if row: 
                         if not any(("[CEL]" in row, "[HEADER]" in row, "[MASKS]" in row, "[OUTLIERS]" in row, "[MODIFIED]" in row)):
-                            #row = [(int(x[0]), int(x[1]), numpy.float64(x[2]), numpy.float64(x[3]), int(x[4])) for x in row]
                             data.append(tuple(row))
-                            #print data[len(data)-1]
                         else:
-                            #self.data = data
                             self.intensity = numpy.array(data, [('x',int),('y',int),('mean',numpy.float64),('stdv',numpy.float64),('npixcels',int)])
                             rsel[row[0]](row, areader)
             
@@ -78,14 +95,13 @@ class affycel:
                 if row:
                     if not any(("[CEL]" in row, "[HEADER]" in row, "[INTESITY]" in row, "[OUTLIERS]" in row, "[MODIFIED]" in row)):
                         
-                        data.append(row)
+                        data.append(tuple(row))
                     else:
-                        self.masks = numpy.array(data, dtype = int)
+                        self.masks = numpy.array(data, [('x',int),('y',int)])
                         rsel[row[0]](row, areader)
             
         def Routliers(row, areader):
             data = []
-            outlierstype = [('x', int), ('y', int)]
             if "[OUTLIERS]" in row:
                             row = areader.next() # moves to the row after "[INTENSITY]"
                             self.outliersCells = int(row[0].partition('=')[2]) #gets the number of intensities
@@ -93,9 +109,9 @@ class affycel:
             for row in reader:
                 if row:
                     if not any(("[CEL]" in row, "[HEADER]" in row, "[INTESITY]" in row, "[MASKS]" in row, "[MODIFIED]" in row)):
-                        data.append(row)
+                        data.append(tuple(row))
                     else:
-                        self.outliers = numpy.array(data, dtype = int)
+                        self.outliers = numpy.array(data, [('x', int), ('y', int)])
                         rsel[row[0]](row, areader)
             
         def Rmodified(row, areader):
@@ -108,29 +124,13 @@ class affycel:
             for row in reader:
                 if row:
                     if not any(("[CEL]" in row, "[HEADER]" in row, "[INTESITY]" in row, "[MASKS]" in row, "[OUTLIERS]" in row)):
-                        data.append(row)
+                        data.append(tuple(row))
                     else:
-                        self.modified[masks] = numpy.array(data, dtype=float64)
-                        #rsel[row[0]](row, areader)
-            
-        rsel = {}
-        rsel['[CEL]'] = Rcel
-        rsel['[HEADER]']= Rheader
-        rsel['[INTENSITY]']= Rintensity
-        rsel['[MASKS]']= Rmasks
-        rsel['[OUTLIERS]']= Routliers
-        rsel['[MODIFIED]']= Rmodified        
+                        self.modified[masks] = numpy.array(data, [('x', int), ('y', int), ('origmean', numpy.float64)] )
+                        #rsel[row[0]](row, areader)  This should be the last item in the file
         
-        def read_selector(areader):
-            for row in areader:
-                print 's1', row
-                if row:
-                    if any(("[CEL]" in row, "[HEADER]" in row, "[INTENSITY]" in row, "[MASKS]" in row, "[OUTLIERS]" in row, "[MODIFIED]" in row)):
-                        print 's2',row
-                        rsel[row[0]](row, areader)
-                    else: print '*****something went wrong*******'
         read_selector(reader)
 
         
-a = affycel()
-a.read_cel("example.CEL")
+#a = affycel()
+#a.read_cel("example.CEL")
